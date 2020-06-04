89ff03c7d190b070339b6083a86ef1aec4f7ad70
Arnedo <arnedojr@gmail.com>
2020-06-03 22:28:19 -0300

    commit inicial

---
+++ android/app/src/main/java/com/barcelar/listatarefasapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.barcelar.listatarefasapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Lista de tarefas", home: Home(),
+  ));
+}
+
+Future<File> _getFile() async {
+
+  return null;
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
